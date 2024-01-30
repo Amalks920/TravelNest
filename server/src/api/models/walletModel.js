4e97c94616a9a40e7394f8dbc335c2e177ffaeb5
amalks <amalks920@gmail.com>
2024-01-30 08:24:52 +0530

    updated

--- server/src/api/models/walletModel.js
+++ server/src/api/models/walletModel.js
@@ -1,5 +1,28 @@
 const mongoose = require('mongoose');
 
+
+const walletHistorySchema = new mongoose.Schema({
+  wallet_id: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Wallet',
+    required: true,
+  },
+  transaction_type: {
+    type: String,
+    enum: ['deposit', 'withdrawal'],
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  // You can add more fields such as transaction date, description, etc. as per your requirements
+}, {
+  timestamps: true, // This adds createdAt and updatedAt fields
+});
+
+const WalletHistory = mongoose.model('WalletHistory', walletHistorySchema);
+
 const walletSchema = new mongoose.Schema({
 
   user_id: {
